b50fd7004d30c5da498eb21008027145dc13f0a0
Iustin Barbir <126501578+IustinBarbir@users.noreply.github.com>
2023-11-05 14:02:17 +0200

    Update MessageCryptor.py

--- Classes/MessageCryptor.py
+++ Classes/MessageCryptor.py
@@ -1,6 +1,6 @@
 class MessageCryptor:
-    def __init__(self, encryption_key):
-        self.encryption_key = encryption_key
+    def __init__(self, cryptosystem_parameters):
+        self.cryptosystem_parameters = cryptosystem_parameters
         self.input_message = None
         self.encrypted_message = None
 
@@ -8,27 +8,20 @@ class MessageCryptor:
         self.input_message = message
 
     def encrypt(self):
-        encrypted_message = []  # Simplified, actual Stern's algorithm is more complex
+        if self.cryptosystem_parameters is not None:
+            encrypted_message = []
 
-        for char in self.input_message:
-            encrypted_char = chr(ord(char) + self.encryption_key)
-            encrypted_message.append(encrypted_char)
+            for char in self.input_message:
+                encrypted_char = chr(ord(char) + self.cryptosystem_parameters.encryption_key)
+                encrypted_message.append(encrypted_char)
 
-        self.encrypted_message = ''.join(encrypted_message)
+            self.encrypted_message = ''.join(encrypted_message)
 
     def decrypt(self):
         decrypted_message = []
 
         for char in self.encrypted_message:
-            decrypted_char = chr(ord(char) - self.encryption_key)
+            decrypted_char = chr(ord(char) - self.cryptosystem_parameters.encryption_key)
             decrypted_message.append(decrypted_char)
 
         return ''.join(decrypted_message)
-
-# Example usage:
-cryptor = MessageCryptor(encryption_key=3)
-cryptor.set_message("Hello, World!")
-cryptor.encrypt()
-print("Encrypted Message:", cryptor.encrypted_message)
-decrypted_message = cryptor.decrypt()
-print("Decrypted Message:", decrypted_message)
