621e0d35522a80a35174d849342dba6b86c9fb64
Iustin Barbir <126501578+IustinBarbir@users.noreply.github.com>
2023-11-05 14:04:24 +0200

    Update relationship.py

--- Classes/relationship.py
+++ Classes/relationship.py
@@ -8,3 +8,20 @@ class CryptosystemParameters:
 
     def set_attack_key(self, attack_key):
         self.attack_key = attack_key
+
+
+# Example usage 
+
+crypto_parameters = CryptosystemParameters(encryption_key=3, attack_key=3)
+
+cryptor = MessageCryptor(crypto_parameters)
+cryptor.set_message("Hello, World!")
+cryptor.encrypt()
+print("Encrypted Message:", cryptor.encrypted_message)
+decrypted_message = cryptor.decrypt()
+print("Decrypted Message:", decrypted_message)
+
+simulator = AttackSimulator(crypto_parameters)
+simulator.intercept_data("Khoor, Zruog!")
+simulator.simulate_attack()
+print("Decrypted Data:", simulator.decrypted_data)
