70adf818e7f765134913fb29368fa1aa97089ea3
Iustin Barbir <126501578+IustinBarbir@users.noreply.github.com>
2023-12-18 10:22:43 +0200

    Update unit_tests.py

--- SternAttack/unit_tests.py
+++ SternAttack/unit_tests.py
@@ -1,6 +1,62 @@
 import unittest
 from unittest.mock import patch
 from attack_stern_algorithm import brute_force_attack
+from attack_stern_algorithm import multiply_matrix
+from attack_stern_algorithm import generate_possible_passwords
+from monitor_aspect import monitor_matrix
+from io import StringIO
+
+class TestMultiplyMatrix(unittest.TestCase):
+
+    def test_multiply_matrix(self):
+        # Test case with known inputs and expected output
+        password = [1, 0, 1]
+        public_key = [
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1]
+        ]
+        expected_result = [0, 0, 0]  # Update the expected result to match your current implementation
+
+        # Calculate the result using multiply_matrix function
+        result = multiply_matrix(password, public_key)
+
+        # Check if the calculated result matches the expected result
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestGeneratePossiblePasswords(unittest.TestCase):
+    def test_generate_possible_passwords(self):
+        # Test case for generate_possible_passwords function
+        length = 3  # Choose a suitable length for testing
+
+        # Expected output: A list of all possible binary combinations of length 3
+        expected_output = [
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 1, 1],
+            [1, 0, 0],
+            [1, 0, 1],
+            [1, 1, 0],
+            [1, 1, 1]
+        ]
+
+        # Obtain the actual output from the function
+        actual_output = generate_possible_passwords(length)
+
+        # Assert whether the actual output matches the expected output
+        self.assertEqual(actual_output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
 
 
 class TestAttackSternAlgorithm(unittest.TestCase):
@@ -25,5 +81,61 @@ class TestAttackSternAlgorithm(unittest.TestCase):
 
         # Check if the brute force attack returns the expected password
         self.assertTrue(True)
-        print("Test 1 passed. ")
+
+
+class TestMonitorMatrix(unittest.TestCase):
+    def test_monitor_matrix_without_anomaly(self):
+        input_matrix_without_anomaly = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 1]
+        ]
+
+        # Simulate no anomaly detection within monitor_matrix
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            # Invoke monitor_matrix with the input matrix
+            output_without_anomaly = monitor_matrix(lambda: input_matrix_without_anomaly)
+            printed_output_without_anomaly = fake_out.getvalue().strip()
+
+            # Ensure there's no printed output about the anomaly
+            self.assertEqual(printed_output_without_anomaly, "")
+
+            # Extract matrix values for comparison
+            output_values = [[elem for elem in row] for row in output_without_anomaly()]
+            input_values = input_matrix_without_anomaly
+
+            self.assertEqual(output_values, input_values)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+"""
+class TestMonitorMatrix(unittest.TestCase):
+    def test_monitor_matrix_with_anomaly(self):
+        # Sample input matrix with an anomaly (non-binary values)
+        input_matrix = [
+            [0, 1, 0],
+            [1, 2, 1],  # Simulating an anomaly value of 2 in the matrix
+            [1, 0, 1]
+        ]
+
+        # Expected output when an anomaly is detected
+        expected_output = "Anomaly found: 2"  # Simulating the expected output message
+
+        # Redirecting stdout to capture the printed message
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            # Call the monitor_matrix function
+            output = monitor_matrix(lambda: input_matrix)  # Mocking the function call
+
+            # Assert whether the printed output contains the expected anomaly message
+            printed_output = fake_out.getvalue().strip()  # Remove leading/trailing whitespaces
+            self.assertEqual(printed_output.strip(), expected_output)
+
+            # Assert that the returned matrix is the same as the input matrix (when an anomaly is detected)
+            self.assertEqual(output, input_matrix)
+
+if __name__ == '__main__':
+    unittest.main()
+"""
 
